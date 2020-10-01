2e4ee7c09642d65974fb3c1048bbc188ef6cf911
jguyon <jguyon@adimeo.com>
2020-10-01 10:01:58 +0200

    [DRUSH] Fix: replaced adimeo-tools deprecated drush log functions by logger()

--- adimeo_tools/src/Drush/AdimeoToolsCommandsTrait.php
+++ adimeo_tools/src/Drush/AdimeoToolsCommandsTrait.php
@@ -11,56 +11,56 @@ namespace Drupal\adimeo_tools\Drush;
  */
 trait AdimeoToolsCommandsTrait {
 
-  /**
-   * Modifie la version d'un module : Ex: drush smv mon_module 8003.
-   *
-   * @param string $moduleName
-   *   Module name.
-   * @param string $version
-   *   Version id.
-   *
-   * @command set_module_version
-   *
-   * @aliases smv
-   */
-  public function setModuleVersion($moduleName = NULL, $version = NULL) {
-    $doAction = TRUE;
-    if (empty($moduleName)) {
-      drush_log('Veuillez indiquer le nom du module', 'error');
-      $doAction = FALSE;
-    }
-    if (empty($version)) {
-      drush_log('Veuillez indiquer la version à appliquer', 'error');
-      $doAction = FALSE;
+    /**
+     * Modifie la version d'un module : Ex: drush smv mon_module 8003.
+     *
+     * @param string $moduleName
+     *   Module name.
+     * @param string $version
+     *   Version id.
+     *
+     * @command set_module_version
+     *
+     * @aliases smv
+     */
+    public function setModuleVersion($moduleName = NULL, $version = NULL) {
+        $doAction = TRUE;
+        if (empty($moduleName)) {
+            $this->logger()->error('Veuillez indiquer le nom du module');
+            $doAction = FALSE;
+        }
+        if (empty($version)) {
+            $this->logger()->error('Veuillez indiquer la version à appliquer');
+            $doAction = FALSE;
+        }
+
+        if ($doAction) {
+            \Drupal::keyValue('system.schema')->set($moduleName, $version);
+            $version = \Drupal::keyValue('system.schema')->get($moduleName);
+            $this->logger()->info(t('Le module @module est désormais en version @version', [
+                '@module'  => $moduleName,
+                '@version' => $version
+            ])->__toString());
+        }
+        else {
+            $this->logger()->info('Ex: drush smv mon_module 8003');
+        }
     }
 
-    if ($doAction) {
-      \Drupal::keyValue('system.schema')->set($moduleName, $version);
-      $version = \Drupal::keyValue('system.schema')->get($moduleName);
-      drush_log(t('Le module @module est désormais en version @version', [
-        '@module'  => $moduleName,
-        '@version' => $version
-      ])->__toString(), 'ok');
+    /**
+     * Recharge la config par défaut d'un module. (très pratique pour les migrations)
+     *
+     * @param string $module
+     *   Nom du module.
+     *
+     * @command reload-module-config
+     *
+     * @aliases rmc
+     */
+    public function reloadModuleConfig($module) {
+        \Drupal::service('config.installer')
+            ->installDefaultConfig('module', $module);
+        $this->logger()->info(t('Install default config of @module has set.', ['@module' => $module]));
     }
-    else {
-      drush_log('Ex: drush smv mon_module 8003', 'ok');
-    }
-  }
-
-  /**
-   * Recharge la config par défault d'un module. (très pratique pour les migrations)
-   *
-   * @param string $module
-   *   Nom du module.
-   *
-   * @command reload-module-config
-   *
-   * @aliases rmc
-   */
-  public function reloadModuleConfig($module) {
-    \Drupal::service('config.installer')
-      ->installDefaultConfig('module', $module);
-    drush_log(t('Install default config of @module has set.', ['@module' => $module]), 'ok');
-  }
 
 }
