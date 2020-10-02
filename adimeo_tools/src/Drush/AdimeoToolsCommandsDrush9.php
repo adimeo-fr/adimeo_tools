ccef82eee3db726844cdf3d65c9f7cd2e803bff5
sgostanyan <sgostanyan@gmail.com>
2020-10-02 12:35:36 +0200

    [DRUSH] Additionnal refactorig for AdimeoToolsCommandsDrush9 class
    Replacement of logger()->info by logger()->notice, Adding Drush 9 compatibility (was locked at Drush 10), Replacement of the use of static Drupal container by service injections.

--- adimeo_tools/src/Drush/AdimeoToolsCommandsDrush9.php
+++ adimeo_tools/src/Drush/AdimeoToolsCommandsDrush9.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\adimeo_tools\Drush;
 
+use Drupal\Core\ProxyClass\Config\ConfigInstaller;
+use Drupal\Core\KeyValueStore\KeyValueFactory;
 use Drush\Commands\DrushCommands;
 
 /**
@@ -12,6 +14,28 @@ use Drush\Commands\DrushCommands;
  * @package Drupal\adimeo_tools\Drush
  */
 class AdimeoToolsCommandsDrush9 extends DrushCommands {
+
+  /**
+   * @var \Drupal\Core\Config\ConfigInstaller
+   */
+  protected $configInstaller;
+
+  /**
+   * @var \Drupal\Core\KeyValueStore\KeyValueFactory
+   */
+  protected $keyValue;
+
+  /**
+   * AdimeoToolsCommandsDrush9 constructor.
+   *
+   * @param \Drupal\Core\ProxyClass\Config\ConfigInstaller $configInstaller
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactory $keyValue
+   */
+  function __construct(ConfigInstaller $configInstaller, KeyValueFactory $keyValue) {
+    parent::__construct();
+    $this->configInstaller = $configInstaller;
+    $this->keyValue = $keyValue;
+  }
 
   use AdimeoToolsCommandsTrait;
 
--- adimeo_tools/src/Drush/AdimeoToolsCommandsTrait.php
+++ adimeo_tools/src/Drush/AdimeoToolsCommandsTrait.php
@@ -11,56 +11,55 @@ namespace Drupal\adimeo_tools\Drush;
  */
 trait AdimeoToolsCommandsTrait {
 
-    /**
-     * Modifie la version d'un module : Ex: drush smv mon_module 8003.
-     *
-     * @param string $moduleName
-     *   Module name.
-     * @param string $version
-     *   Version id.
-     *
-     * @command set_module_version
-     *
-     * @aliases smv
-     */
-    public function setModuleVersion($moduleName = NULL, $version = NULL) {
-        $doAction = TRUE;
-        if (empty($moduleName)) {
-            $this->logger()->error('Veuillez indiquer le nom du module');
-            $doAction = FALSE;
-        }
-        if (empty($version)) {
-            $this->logger()->error('Veuillez indiquer la version à appliquer');
-            $doAction = FALSE;
-        }
-
-        if ($doAction) {
-            \Drupal::keyValue('system.schema')->set($moduleName, $version);
-            $version = \Drupal::keyValue('system.schema')->get($moduleName);
-            $this->logger()->info(t('Le module @module est désormais en version @version', [
-                '@module'  => $moduleName,
-                '@version' => $version
-            ])->__toString());
-        }
-        else {
-            $this->logger()->info('Ex: drush smv mon_module 8003');
-        }
+  /**
+   * Modifie la version d'un module : Ex: drush smv mon_module 8003.
+   *
+   * @param string $moduleName
+   *   Module name.
+   * @param string $version
+   *   Version id.
+   *
+   * @command set_module_version
+   *
+   * @aliases smv
+   */
+  public function setModuleVersion($moduleName = NULL, $version = NULL) {
+    $doAction = TRUE;
+    if (empty($moduleName)) {
+      $this->logger()->error('Veuillez indiquer le nom du module');
+      $doAction = FALSE;
+    }
+    if (empty($version)) {
+      $this->logger()->error('Veuillez indiquer la version à appliquer');
+      $doAction = FALSE;
     }
 
-    /**
-     * Recharge la config par défaut d'un module. (très pratique pour les migrations)
-     *
-     * @param string $module
-     *   Nom du module.
-     *
-     * @command reload-module-config
-     *
-     * @aliases rmc
-     */
-    public function reloadModuleConfig($module) {
-        \Drupal::service('config.installer')
-            ->installDefaultConfig('module', $module);
-        $this->logger()->info(t('Install default config of @module has set.', ['@module' => $module]));
+    if ($doAction) {
+      $this->keyValue->get('system.schema')->set($moduleName, $version);
+      $version = $this->keyValue->get('system.schema')->get($moduleName);
+      $this->logger()->notice(t('Le module @module est désormais en version @version', [
+        '@module'  => $moduleName,
+        '@version' => $version
+      ])->__toString());
     }
+    else {
+      $this->logger()->notice('Ex: drush smv mon_module 8003');
+    }
+  }
+
+  /**
+   * Recharge la config par défaut d'un module. (très pratique pour les migrations)
+   *
+   * @param string $module
+   *   Nom du module.
+   *
+   * @command reload-module-config
+   *
+   * @aliases rmc
+   */
+  public function reloadModuleConfig($module) {
+    $this->configInstaller->installDefaultConfig('module', $module);
+    $this->logger()->notice(t('Install default config of @module has set.', ['@module' => $module]));
+  }
 
 }
