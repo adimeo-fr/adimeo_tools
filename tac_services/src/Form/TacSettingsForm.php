f9c0fea3d744e9e74832c834aa23c31e60f58611
Adam Carton de Wiart <acartondewiart@adimeo.com>
2021-04-01 22:43:05 +0200

    [TAC] fix minor issue in global settings form

--- tac_services/src/Form/TacSettingsForm.php
+++ tac_services/src/Form/TacSettingsForm.php
@@ -124,7 +124,7 @@ class TacSettingsForm extends FormBase {
     $form[$this->config::ORIENTATION] = [
       '#type'          => 'select',
       '#title'         => t("Position de la banniere de cookies"),
-      '#default_value' => $defaultValues[$this->config::ICON_POSITION],
+      '#default_value' => $defaultValues[$this->config::ORIENTATION],
       '#description'   => t("Celle-ci est affiche la premiere fois qu'un visiteur arrive sur le site"),
       '#options'       => [
         'bottom'    => t('En Bas'),
