9d39b0255f37fde0ba08870d9203a4a7176c540f
Quentin Rinaldi <quentin.rinaldi@gmail.com>
2022-11-10 14:56:48 +0100

    [TAC SERVICES] evol - removed unusable mathtag service and added math tag cookie name in tac gtag service filter

--- tac_services/libraries/tarteaucitron/tarteaucitron.services.js
+++ tac_services/libraries/tarteaucitron/tarteaucitron.services.js
@@ -1712,7 +1712,7 @@ tarteaucitron.services.googletagmanager = {
     "name": "Google Tag Manager",
     "uri": "https://adssettings.google.com/",
     "needConsent": true,
-    "cookies": ['_ga', '_gat', '__utma', '__utmb', '__utmc', '__utmt', '__utmz', '__gads', '_drt_', 'FLC', 'exchange_uid', 'id', 'fc', 'rrs', 'rds', 'rv', 'uid', 'UIDR', 'UID', 'clid', 'ipinfo', 'acs'],
+    "cookies": ['_ga', '_gat', '__utma', '__utmb', '__utmc', '__utmt', '__utmz', '__gads', '_drt_', 'FLC', 'exchange_uid', 'id', 'fc', 'rrs', 'rds', 'rv', 'uid', 'UIDR', 'UID', 'clid', 'ipinfo', 'acs', 'mt_mop','mt_misc','uuid'],
     "js": function () {
         "use strict";
         if (tarteaucitron.user.googletagmanagerId === undefined) {
