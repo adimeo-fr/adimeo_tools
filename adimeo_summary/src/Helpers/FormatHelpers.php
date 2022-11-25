c25ccfbc9b84d17c712ab457b0a03278e5bb9f09
Vivien <vivien.barbeau.contact@gmail.com>
2022-11-25 15:39:25 +0100

    [ADIMEO_SUMMARY] Update module because of this issue "Calling static trait member directly is deprecated. It should only be accessed on a class using the trait."

---
+++ adimeo_summary/src/Helpers/FormatHelpers.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\adimeo_summary\Helpers;
+
+/**
+ * Class that contains globally reusable methods
+ */
+class FormatHelpers {
+
+  public static function stringToKebabCase(string $text): string {
+    return str_replace(' ', '-', strtolower($text));
+  }
+
+  public static function unAccent(string $string): string {
+    $search = ['À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ'];
+    $replace = ['A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y'];
+    return str_replace($search, $replace, $string);
+  }
+
+  public static function stringToFirstLetterUppercase(string $text): string {
+    return ucfirst(strtolower($text));
+  }
+
+}
